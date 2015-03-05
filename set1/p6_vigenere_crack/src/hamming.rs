0e69a21a85167e84b50e8a1cd4bac5d8f72d9fcb
Devon Hollowood <devonhollowood@gmail.com>
2015-03-05 02:42:47 -0800

    Added set 1 problem 6; got hamming number working

---
+++ set1/p6_vigenere_crack/src/hamming.rs
@@ -0,0 +1,42 @@
+fn n_ones(n : u8) -> u8 {
+    let mut ones=0;
+    let mut val=n;
+    while val != 0 {
+        ones += 1;
+        val &= val-1;
+    }
+    ones
+}
+
+#[test]
+fn n_ones_test() {
+    assert_eq!(n_ones(5), 2);
+}
+
+pub fn hamming_dist(a: &[u8], b:&[u8]) -> Option<u32> {
+    use std::iter::{IteratorExt, AdditiveIterator};
+    if a.len()!=b.len() {
+        return None;
+    }
+    let result = a.iter().zip(b.iter()).map(|(x,y)| {n_ones(x^y) as u32}).sum();
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hamming_eq_test() {
+        let a = "this is a test".as_bytes();
+        let b = "wokka wokka!!!".as_bytes();
+        assert_eq!(Some(37), hamming_dist(a,b));
+    }
+
+    #[test]
+    fn hamming_none_test() {
+        let a = "this is a test".as_bytes();
+        let b = "this is not a test".as_bytes();
+        assert_eq!(None, hamming_dist(a,b));
+    }
+}
