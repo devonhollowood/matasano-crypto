c9d0e83b5d641128c02994686c7afd3c7ac54604
Devon Hollowood <devonhollowood@gmail.com>
2015-07-13 22:49:46 +0100

    Completed set 3 problem 19

--- set3/p19_break_ctr_substitutions/src/main.rs
+++ set3/p19_break_ctr_substitutions/src/main.rs
@@ -5,20 +5,22 @@ mod aes;
 mod ctr;
 mod score;
 
+use std::io;
+
 fn crack(ciphertexts: &Vec<Vec<u8>>) -> Vec<u8> {
-    let max_len = ciphertexts.iter().map(|v| v.len()).max();
-    let keystream = Vec::new();
+    let max_len = ciphertexts.iter().map(|v| v.len()).max().unwrap();
+    let mut keystream = Vec::new();
     // I believe the text to only consist of characters in the range 0x20-0x7e,
     // so each character will be screened to make sure it in in this range
     let valid_char = |c| c >= 0x20 && c <= 0x7e;
     //take ciphertexts one index at a time
     for idx in 0..max_len {
-        let best_byte = 0u8;
-        let best_score = 0f32;
+        let mut best_byte = 0u8;
+        let mut best_score = 0f32;
         // try each possible byte and select the best one
         'bytes: for test_byte in (0..256).map(|x| x as u8) {
             // get letters from this test byte
-            let letters = String::new();
+            let mut letters = String::new();
             for ciphertext in ciphertexts{
                 if idx < ciphertext.len() {
                     let xor = test_byte ^ ciphertext[idx];
@@ -30,7 +32,8 @@ fn crack(ciphertexts: &Vec<Vec<u8>>) -> Vec<u8> {
                     }
                 }
             }
-            if score::score(&letters[..]) < best_score || best_score==0 {
+            let score = score::score(&letters[..]);
+            if score < best_score || best_score == 0f32 {
                 best_score = score;
                 best_byte = test_byte;
             }
@@ -50,7 +53,7 @@ fn base64_lines(filename: &str) -> io::Result<Vec<Vec<u8>>> {
     let mut contents = String::new();
     try!(file.read_to_string(&mut contents));
     //base64 decode
-    let lines = Vec::new();
+    let mut lines = Vec::new();
     for line in contents.lines() {
         match line[..].from_base64() {
             Ok(vec) => lines.push(vec),
@@ -59,7 +62,7 @@ fn base64_lines(filename: &str) -> io::Result<Vec<Vec<u8>>> {
                                  ),
         }
     }
-    lines
+    Ok(lines)
 }
 
 fn main() {
@@ -77,17 +80,17 @@ fn main() {
             return;
         }
     }
-    let ciphertexts = Vec::new();
+    let mut ciphertexts = Vec::new();
     let nonce = 0;
     let key = "YELLOW SUBMARINE".as_bytes();
     for line in lines {
-        let encryptor = AesCtr::new(nonce, &key[..]);
-        ciphertexts.push(encryptor.encrypt(line[..]).unwrap());
+        let mut encryptor = ctr::AesCtr::new(nonce, &key[..]);
+        ciphertexts.push(encryptor.encrypt(&line[..]).unwrap());
     }
     let keystream = crack(&ciphertexts);
     for ciphertext in ciphertexts {
         for idx in 0..ciphertext.len() {
-            print!("{}", ciphertext[idx] ^ keystream[idx] as char);
+            print!("{}", (ciphertext[idx] ^ keystream[idx]) as char);
         }
         println!("");
     }
