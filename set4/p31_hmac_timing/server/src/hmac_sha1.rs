01858079d9b7436477c84ce0b8e14c1090fc05ab
Devon Hollowood <devonhollowood@gmail.com>
2015-09-12 23:32:03 -0300

    Reorganised code for set 4 problem 31

---
+++ set4/p31_hmac_timing/server/src/hmac_sha1.rs
@@ -0,0 +1,88 @@
+pub fn hmac_sha1(key: &[u8], message: &[u8]) -> [u8; 20] {
+    use std::iter::repeat;
+    //get key to correct length
+    let prepared_key = prepare_key(key);
+    let outer_pad: Vec<u8> =
+        repeat(0x5c).zip(prepared_key.iter()).map(|(x, y)| x ^ y).collect();
+    let inner_pad: Vec<u8> =
+        repeat(0x36).zip(prepared_key.iter()).map(|(x, y)| x ^ y).collect();
+    let inner: Vec<u8> = inner_pad.iter().chain(message.iter()).cloned()
+                                  .collect();
+    let outer: Vec<u8> = outer_pad.iter().chain(sha1(&inner[..]).iter())
+                                  .cloned().collect();
+    sha1(&outer[..])
+}
+
+///SHA1 of `input`
+fn sha1(input: &[u8]) -> [u8; 20] {
+    use crypto::digest::Digest;
+    use crypto::sha1::Sha1;
+    let mut hasher = Sha1::new();
+    hasher.input(input);
+    let mut output = [0; 20];
+    hasher.result(& mut output[..]);
+    output
+}
+
+///Prepares key for HMAC-SHA1
+fn prepare_key(key: &[u8]) -> [u8; 64] {
+    use std::io::Write;
+    const BLOCKSIZE: usize = 64;
+    let mut prepared_key = [0; 64];
+    if key.len() <= BLOCKSIZE { //zero-pad key
+        (&mut prepared_key[..]).write_all(key).unwrap();
+    }
+    else if key.len() > BLOCKSIZE { //hash key
+        (&mut prepared_key[..]).write_all(&sha1(key)[..]).unwrap();
+    }
+    prepared_key
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn hmac_sha1() {
+        let key = b"yellow submarine";
+        let message = b"In the town / where I was born / there lived a man / \
+                        who sailed to sea";
+        let expected = [0x7f, 0x58, 0xc4, 0xf7, 0xc7, 0x94, 0xba, 0xa2, 0x5e,
+                        0xcd, 0xbe, 0x90, 0x19, 0x1e, 0x45, 0xe3, 0x5e, 0xd6,
+                        0x99, 0xff];
+        assert_eq!(super::hmac_sha1(key, message), expected);
+    }
+
+    #[test]
+    fn sha1() {
+        let expected = [0xda, 0x03, 0x5e, 0x50, 0x1d, 0xef, 0x35, 0x4e, 0xc3,
+                        0x12, 0x5f, 0x4b, 0xb1, 0x28, 0xeb, 0x3d, 0xfb, 0x28,
+                        0x42, 0xb7];
+        assert_eq!(super::sha1(b"yellow submarine"), expected);
+    }
+
+    #[test]
+    fn prepare_key() {
+        //short key
+        assert_eq!(&super::prepare_key(b"yellow submarine")[..],
+                   &b"yellow submarine\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
+                     \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
+                     \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
+                     \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"[..]);
+        //long key
+        let long_input = b"In the town / where I was born / there lived a man / \
+                           who sailed the seas";
+        let long_expected = [0xf5, 0xc1, 0x17, 0xa6, 0xcf, 0x2e, 0xea, 0x95,
+                             0xef, 0x99, 0x06, 0x3e, 0xe9, 0x9e, 0x2d, 0x27,
+                             0x9d, 0x6a, 0xbb, 0x79, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        assert_eq!(&super::prepare_key(long_input)[..], &long_expected[..]);
+        //64-byte key
+        let exact_key = b"yellow submarineyellow submarineyellow submarine\
+                          yellow submarine";
+        assert_eq!(&super::prepare_key(exact_key)[..], &exact_key[..]);
+    }
+}
---
+++ set4/p31_hmac_timing/server/src/main.rs
@@ -0,0 +1,141 @@
+extern crate crypto;
+extern crate hyper;
+extern crate rustc_serialize;
+extern crate rand;
+
+mod hmac_sha1;
+
+use hyper::server::{Server, Request, Response};
+use hyper::status::StatusCode;
+use hyper::net::Fresh;
+use hyper::uri::RequestUri::AbsolutePath;
+
+const HOST: &'static str = "localhost:9000";
+
+fn main() {
+    let key = gen_key();
+    println!("Key: {} (len {})", format_hex(&key[..]), key.len());
+    let server = Server::http(HOST).unwrap();
+    println!("test.txt hmac: {} (Shhhh!)",
+             format_hex(&file_hmac(&key[..], "test.txt").unwrap()[..]));
+    println!("Listening on port 9000");
+    server.handle(
+        move |req: Request, res: Response| {
+            handle_request(&key[..], req, res)
+        }
+    ).unwrap();
+}
+
+fn format_hex(hex: &[u8]) -> String {
+    use std::fmt::Write;
+    let mut s = String::new();
+    for el in hex.iter() {
+        write!(&mut s, "{:02x}", el).unwrap();
+    }
+    s
+}
+
+fn gen_key() -> Vec<u8> {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    let key_len = rng.gen_range(10, 256);
+    rng.gen_iter().take(key_len).collect()
+}
+
+fn handle_request(key: &[u8], req: Request, mut res: Response<Fresh>) {
+    match req.method {
+        hyper::Get => {
+            match req.uri {
+                AbsolutePath(path) => *res.status_mut() = handle_path(key, &path[..]),
+                _ => *res.status_mut() = StatusCode::NotFound,
+            }
+        },
+        _ => *res.status_mut() = StatusCode::MethodNotAllowed,
+    }
+    send_response(res);
+}
+
+fn handle_path(key: &[u8], path: &str) -> StatusCode {
+    let full_path = format!("http://{}/{}", HOST, path);
+    match hyper::Url::parse(&full_path[..]).ok().and_then(|url| url.query_pairs()) {
+        Some(pairs) => {
+            if pairs.len() == 2 {
+                let (ref arg1, ref filename) = pairs[0];
+                let (ref arg2, ref signature) = pairs[1];
+                if &arg1[..]=="file" && &arg2[..]=="signature" {
+                        check_signature(key, &filename[..], &signature[..])
+                    }
+                else { StatusCode::BadRequest }
+            }
+            else { StatusCode::BadRequest }
+        },
+        _ => StatusCode::NotFound,
+    }
+}
+
+fn send_response(res: Response) {
+    match res.status() {
+        StatusCode::Ok =>
+            { res.send(b"<h1>Server says everything is a-okay</h1>\n").unwrap(); },
+        StatusCode::BadRequest =>
+            { res.send(b"<h1>400: Bad Request</h1>\n").unwrap(); },
+        StatusCode::NotFound =>
+            { res.send(b"<h1>404: Not Found</h1>\n").unwrap(); },
+        StatusCode::MethodNotAllowed =>
+            { res.send(b"<h1>405: Method Not Allowed</h1>\n").unwrap(); },
+        StatusCode::InternalServerError =>
+            { res.send(b"<h1>500: Internal Server Error</h1>\n").unwrap(); },
+        _ => {},
+    }
+}
+
+fn check_signature(key: &[u8], filename: &str, signature: &str) -> StatusCode {
+    use rustc_serialize::hex::FromHex;
+    let parsed_signature = match signature.from_hex() {
+        Ok(sig) => sig,
+        _ => return StatusCode::BadRequest,
+    };
+    let file_hash = match file_hmac(key, filename) {
+        Ok(sha1) => sha1,
+        _ => return StatusCode::NotFound,
+    };
+    if insecure_compare(&file_hash[..], &parsed_signature[..]) {
+        StatusCode::Ok
+    }
+    else {
+        StatusCode::InternalServerError
+    }
+}
+
+fn file_hmac(key: &[u8], filename: &str) -> std::io::Result<[u8; 20]> {
+    use std::io::prelude::*;
+    use std::fs::File;
+    let mut file = try!(File::open(filename));
+    let mut s = String::new();
+    try!(file.read_to_string(&mut s));
+    Ok(hmac_sha1::hmac_sha1(key, &s.into_bytes()[..]))
+}
+
+fn insecure_compare(first: &[u8], second: &[u8]) -> bool {
+    for (x, y) in first.iter().zip(second.iter()) {
+        if { x != y } { return false; }
+        std::thread::sleep_ms(50);
+    }
+    if first.len() != second.len() { //do this after step-by-step to preserve
+        return false;                //element-by-element comparison
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test] #[ignore]
+    fn insecure_compare() {
+        assert!(super::insecure_compare(b"yellow submarine", b"yellow submarine"),
+            "should have been equal");
+        assert!(!super::insecure_compare(b"yellow submarine", b"yellow_submarine"),
+            "should have been unequal");
+    }
+
+}
